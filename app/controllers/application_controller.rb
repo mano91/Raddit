f9d0eb87bed8a7b400acbc3393eff75aff050efe
mano91 <mano.c247@gmail.com>
2015-05-19 14:35:42 +0530

    raddit
    first app

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,30 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  
+
+
+ def upvote
+  @link = Link.find(params[:id])
+  @link.upvote_by current_user
+  redirect_to :back
+end
+ 
+def downvote
+  @link = Link.find(params[:id])
+  @link.downvote_by current_user
+  redirect_to :back
+end  
+
+
+  protect_from_forgery with: :exception
+   before_action :configure_permitted_parameters, if: :devise_controller?
+     
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :name
+    devise_parameter_sanitizer.for(:account_update) << :name
+  end
+
+end
